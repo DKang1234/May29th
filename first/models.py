d6f1e7c3d2fe1d253d2efba940e163d5c08c4281
Danny <danwizblue1234@gmail.com>
2021-06-05 15:03:16 +0900

    June5th

--- first/models.py
+++ first/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
-    email   = models.EmailField(max_length=264, unique=True)
+    email   = models.EmailField(max_length=264, unique=True)   
+    password = models.CharField(max_length=128, default="") 
